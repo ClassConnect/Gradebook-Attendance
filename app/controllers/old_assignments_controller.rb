c68ed4a8193f74e1c60908455f08338b35ad80cc
Ted Kalaw <ted.kalaw@gmail.com>
2011-07-04 15:31:14 -0700

    Popups work, with link to openbox edit.

---
+++ app/controllers/old_assignments_controller.rb
@@ -0,0 +1,45 @@
+class AssignmentsController < ApplicationController
+  #GET /gradebooks/:course_id/:assignment_id
+  def edit
+    @assignment = Assignment.find(params[:assignment_id])
+    @types = AssignmentType.where(:course_id => params[:course_id])
+  end
+
+  #PUT /gradebooks/:course_id/:assignment_id
+    def update
+    puts params
+    @assignment = Assignment.find(params[:assignment][:id])
+    respond_to do |format|
+      if @assignment.update_attributes(params[:assignment])
+        format.js
+      else
+        format.js
+      end
+    end
+  end
+
+  def new
+    @course = Course.find(params[:course_id])
+    @assignment = Assignment.new
+    @types = AssignmentType.where(:course_id => params[:course_id])
+    respond_to do |format|
+      format.html
+    end
+  end
+
+
+  def create
+    @assignment = Assignment.new(params[:assignment])
+    respond_to do |format|
+      if @assignment.save
+        @assignment.init_grades
+        format.html
+        format.js
+      else
+        format.html
+        format.js
+      end
+    end
+  end
+
+end
