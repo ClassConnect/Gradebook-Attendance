3ff7072c74139106713509469f65898e10b657a0
Ted Kalaw <ted.kalaw@gmail.com>
2011-08-13 20:13:11 -0700

    Working on auth

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -1,10 +1,12 @@
 class GradebooksController < ApplicationController
 
-
+  before_filter :authenticate_user!
+  
   #Standard 10-point scale
   DEFAULT_SCALE = [{:from => 0,:to => 59, :name =>"F"}, {:from=>60,:to=>69,:name=>"D"}, {:from=>70,:to =>79,:name =>"C"}, {:from =>80,:to=>89,:name=>"B"},{:from=>90,:to=>100,:name=>"A"}]
   
   def show
+    teaches_class?(params[:course_id])
     @course = Course.find(params[:course_id])
     @students = @course.students.order_names
     @assignments = @course.assignments.asc(:created_at).cache
@@ -38,10 +40,11 @@ class GradebooksController < ApplicationController
 
   private
   def teaches_class?()
-    selected_courses = @current_user.courses.select("id");
+    selected_courses = current_user.courses.select("id");
     puts "HI"
-    logger.debug(@current_user.id)
-
+    puts current_user.id
+    puts(selected_courses)
+    logger.debug(current_user.id)
     logger.debug(selected_courses)
   end
 end
