57c043171619b8d21ac2a34f4777b59ee3a47d14
Ted Kalaw <ted.kalaw@gmail.com>
2011-08-14 02:28:06 -0700

    ...

--- app/assets/javascripts/jquery.dataTables.js
+++ app/assets/javascripts/jquery.dataTables.js
@@ -968,7 +968,7 @@
 				"sProcessing": "Processing...",
 				"sLengthMenu": "Show _MENU_ entries",
 				"sZeroRecords": "No matching records found",
-				"sEmptyTable": "No data available in table",
+				"sEmptyTable": "There are no students in this class",
 				"sLoadingRecords": "Loading...",
 				"sInfo": "Showing _START_ to _END_ of _TOTAL_ entries",
 				"sInfoEmpty": "Showing 0 to 0 of 0 entries",
