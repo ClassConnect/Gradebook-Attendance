f8679e38054b24a2c9464efc7484b637f3e2462b
Ted Kalaw <ted.kalaw@gmail.com>
2011-08-13 23:07:59 -0700

    Hopefully fixed..

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,6 +1,6 @@
 module AssignmentsHelper
   def id_to_link
-    return "gradebooks/" + @assignment.course_id.to_s + '/' + @assignment.id.to_s + '/edit'
+    return "/gradebooks/" + @assignment.course_id.to_s + '/' + @assignment.id.to_s + '/edit'
   end
 
   def test_helper(test_var)
