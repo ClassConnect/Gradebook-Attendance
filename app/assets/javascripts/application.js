aff8417344082f3ffc38ad34e551af53f7490128
Ted Kalaw <ted.kalaw@gmail.com>
2011-08-27 23:10:50 -0700

    Forgot to comment out jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
+// only for dev...require jquery
 //= require jquery_ujs
 //= require jquery.dataTables
 //= require_tree .
