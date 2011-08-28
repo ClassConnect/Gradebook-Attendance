078496242f4ed428c65bafba609d45d0b01eee55
Scott Rice <meris608@gmail.com>
2011-08-28 13:42:58 -0700

    Edited app/controllers/application_controller.rb via GitHub

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::Base
       session[:session_key] = params[:session_id]
     end
     if session[:session_key].nil?
-      return redirect_to "/app/home.cc"
+      return redirect_to "http://www.classconnect.com/app/home.cc"
     end
     key = UsersKey.where(:session_key => session[:session_key]).first
     if key.nil?
-      return redirect_to "/app/home.cc"
+      return redirect_to "http://www.classconnect.com/app/home.cc"
     end
     @current_user = key.user 
   end
